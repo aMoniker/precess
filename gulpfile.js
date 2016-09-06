91db9fb4696b254ce460414085f8b558da8b6815
Jim Greenleaf <james.a.greenleaf@gmail.com>
2016-09-06 01:06:02 -0400

    update gulpfile to process modules directory

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,21 @@
 var gulp = require('gulp');
 var screeps = require('gulp-screeps');
+var creds = require('./account-credentials.js');
 
-gulp.task('default', [], function() {});
+function pushCode() {
+    return gulp.src(['main.js', 'modules/*.js'])
+               .pipe(screeps(creds))
+               ;
+};
+
+gulp.task('default', ['live']);
+gulp.task('screeps', pushCode);
+
+// update screeps live code
+gulp.task('live', function() {
+    gulp.watch([
+        '**/*.js',
+        '!gulpfile.js',
+        '!node_modules/**/*.js'
+    ], ['screeps']);
+});
